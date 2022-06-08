5035fbb8cf7dd9643e76f37de2d969d4f2c5ef8c
seong10 <kck555511@naver.com>
2022-06-08 14:27:34 +0900

    add

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import pickle
 
 from googletrans import Translator
 
+# 레이아웃 과정
 st.set_page_config(layout="wide")
 empty1,con0,empty2 = st.columns([1,1.0,0.9])
 empty1,con0_1,empty2 = st.columns([0.6,0.5,0.3])
@@ -21,19 +22,19 @@ def main() :
     
     with con0 :
         st.image('https://thumbs.gfycat.com/GaseousBlueColt.webp')
-        
+                       
     with con0_1 :
         st.title('영화 추천')
         st.title('')
 
     with con1 :
         df_corr = pd.read_csv('data/df_corr.csv', index_col=0)
-
         col = df_corr.columns
+
         choice_movie = st.selectbox('영화 선택', col)
 
     with con2 :
-
+        # 크롤링한 이미지URL 데이터를 dict에 저장 했고, 불러오는 과정
         with open('data/url_dict.pickle', 'rb') as f:
             url_dict = pickle.load(f)
 
@@ -41,7 +42,8 @@ def main() :
         st.image(url_dict)
 
     with con3 :
-        
+        # 데이터의 줄거리 부분을 가져왔고
+        # 구글 번역 API를 통해 변역함
         st.text('줄거리')
 
         df_overview = pd.read_csv('data/df_overview.csv', index_col=0)
@@ -62,10 +64,12 @@ def main() :
    
     with con5 :
 
+        # 유저의 별점 저장하는 과정
         user_rating_reset = pd.read_csv('data/user_rating_reset.csv', index_col= 0)
         user_rating = pd.read_csv('data/user_rating.csv', index_col= 0)
-
+        
         if st.button('저장하기') :
+            
             if ( user_rating.loc[ user_rating['userId'] == 999 , ]['userId'].tolist() ) == [] :
                 user_rating = user_rating_reset.append( { 'title' : choice_movie , 'userId' : 999, 'rating' : float(choice_rating) }, ignore_index=True )
                 user_rating.to_csv('data/user_rating.csv')
@@ -73,7 +77,8 @@ def main() :
             elif ( ( (user_rating.loc[ user_rating['userId'] == 999 , ])['userId'] == 999).tolist() )[0] :
                 user_rating = user_rating.append( { 'title' : choice_movie , 'userId' : 999, 'rating' : float(choice_rating) }, ignore_index=True )
                 user_rating.to_csv('data/user_rating.csv')
-                
+
+        # 저장된 데이터를 삭제하는 과정        
         if st.checkbox('삭제하기') :
 
             if st.button('리셋하기') :
@@ -94,10 +99,10 @@ def main() :
         st.dataframe(user_rating)
 
         st.title('')
-        if st.button('내 영화들의 추천 영화 보기') :
+        if st.button('내가 본 영화들의 추천 영화 보기') :
             st.text('추천 영화 5개')
 
-            df_corr = pd.read_csv('data/df_corr.csv', index_col='title')
+            # 내가 영화에 준 별점을 상관계수에 채우는 과정
             similar_movies_list = pd.DataFrame()
 
             for i in range( user_rating.shape[0] ) :
@@ -107,7 +112,7 @@ def main() :
                 recom_movies['weight'] = user_rating['rating'][i] * recom_movies['correlation']
                 similar_movies_list = similar_movies_list.append( recom_movies )
 
-            # 내 영화들 지우고 추천영화만 정렬해서 보여주는 작업
+            # 내 영화들 지우고 추천영화만 정렬해서 보여주는 과정
             similar_movies_list = similar_movies_list.sort_values('weight' , ascending=False)
             user_rating['title'].to_list()
             similar_movies_list.drop( user_rating['title'].to_list() , axis=0,  inplace= True )
